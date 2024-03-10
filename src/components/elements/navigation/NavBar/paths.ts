a79bce5c833501903757952ff3e07a6cdede8623
Abdul Arif <55670928+abdularif0705@users.noreply.github.com>
2024-03-10 00:15:26 -0500

    Refactor navigation paths in NavBar component

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -1,44 +1,62 @@
 import { ISubNav } from '../SubNav/SubNav';
 
 const NAV_LINKS: ISubNav[] = [
-  { label: 'Home', href: '/' },
+  { label: "Home", href: "/" },
   {
-    label: 'Transactions',
-    href: '/transactions',
+    label: "Transactions",
+    href: "/transactions",
   },
   {
-    label: 'Transfers',
-    href: '/transfers',
+    label: "Send/Receive",
+    href: "/send-receive",
     children: [
       {
-        label: 'ERC20',
-        subLabel: 'Get your ERC20 transfers',
-        href: '/transfers/erc20',
-        logo: 'token',
+        label: "ERC20",
+        subLabel: "Get your ERC20 transfers",
+        href: "/send-receive/erc20",
+        logo: "token",
       },
       {
-        label: 'NFT',
-        subLabel: 'Get your ERC721 an ERC1155 transfers',
-        href: '/transfers/nft',
-        logo: 'lazyNft',
+        label: "NFT",
+        subLabel: "Get your ERC721 an ERC1155 transfers",
+        href: "/send-receive/nft",
+        logo: "lazyNft",
       },
     ],
   },
   {
-    label: 'Balances',
-    href: '/balances',
+    label: "Transfers",
+    href: "/transfers",
     children: [
       {
-        label: 'ERC20',
-        subLabel: 'Get your ERC20 balances',
-        href: '/balances/erc20',
-        logo: 'token',
+        label: "ERC20",
+        subLabel: "Get your ERC20 transfers",
+        href: "/transfers/erc20",
+        logo: "token",
       },
       {
-        label: 'NFT',
-        subLabel: 'Get your ERC721 an ERC1155 balances',
-        href: '/balances/nft',
-        logo: 'pack',
+        label: "NFT",
+        subLabel: "Get your ERC721 an ERC1155 transfers",
+        href: "/transfers/nft",
+        logo: "lazyNft",
+      },
+    ],
+  },
+  {
+    label: "Balances",
+    href: "/balances",
+    children: [
+      {
+        label: "ERC20",
+        subLabel: "Get your ERC20 balances",
+        href: "/balances/erc20",
+        logo: "token",
+      },
+      {
+        label: "NFT",
+        subLabel: "Get your ERC721 an ERC1155 balances",
+        href: "/balances/nft",
+        logo: "pack",
       },
     ],
   },
