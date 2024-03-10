e3701c3c5813fe5aee84a8a4749fada646739eee
Abdul Arif <55670928+abdularif0705@users.noreply.github.com>
2024-03-10 00:19:22 -0500

    Update subLabels for ERC20 and NFT in NavBar

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -12,13 +12,13 @@ const NAV_LINKS: ISubNav[] = [
     children: [
       {
         label: "ERC20",
-        subLabel: "Get your ERC20 transfers",
+        subLabel: "Transfer ERC20 tokens between addresses",
         href: "/send-receive/erc20",
         logo: "token",
       },
       {
         label: "NFT",
-        subLabel: "Get your ERC721 an ERC1155 transfers",
+        subLabel: "Transfer ERC721 and ERC1155 assets between addresses",
         href: "/send-receive/nft",
         logo: "lazyNft",
       },
