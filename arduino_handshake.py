8e917c4b66d89e876dfae89034777caa69af5f41
CameronTenkel <118020522+CameronTenkel@users.noreply.github.com>
2024-03-10 10:45:57 -0400

    update python

--- arduino_handshake.py
+++ arduino_handshake.py
@@ -6,7 +6,7 @@ baud_rate = 9600
 
 def send_function(serial_port, receive_wallet, send_wallet, eth_amount) -> bool:
     # Open the serial port
-    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+    ser = serial.Serial(serial_port, baud_rate, timeout=0)
 
     message = 't' + receive_wallet + send_wallet
     ser.write(message.encode())
@@ -20,7 +20,7 @@ def send_function(serial_port, receive_wallet, send_wallet, eth_amount) -> bool:
 
 def receive_function(serial_port, send_wallet, receive_wallet, eth_amount) -> bool:
     # Open the serial port
-    ser = serial.Serial(serial_port, baud_rate, timeout=1)
+    ser = serial.Serial(serial_port, baud_rate, timeout=0)
 
     message = 'r' + send_wallet + receive_wallet
     ser.write(message.encode())
@@ -41,10 +41,10 @@ if __name__ == '__main__':
     send_wallet_address = "11Ec4aF7f5e10068Cc07EDDC6884223a0b93b732" # no 0x in front because it is an arbitrary hex tag
 
     if action == 'send':
-        return_bool = send_function("COM10", recipient, send_wallet_address, amount)
+        return_bool = send_function("COM10", recipient[2:], send_wallet_address, amount)
         print(return_bool)
     elif action == 'receive':
-        return_bool = receive_function("COM10", send_wallet_address, recipient, amount)
+        return_bool = receive_function("COM10", send_wallet_address, recipient[2:], amount)
         print(return_bool)
     else:
         print("Invalid action. Please specify 'send' or 'receive'.")
